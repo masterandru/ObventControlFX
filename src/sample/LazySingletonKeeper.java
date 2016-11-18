8956bdfd33f3783e9bb8028664d6bdec9faa6cb1
andruschenco <andruschenco@mail.ru>
2016-11-18 15:37:18 +0300

    08-11-2016 - add LazySingelton class

--- src/sample/LazySingletonKeeper.java
+++ src/sample/LazySingletonKeeper.java
@@ -3,6 +3,7 @@ package sample;
 /**
  * Created by andruschenko-ip on 18.11.2016.
  */
+
 public class LazySingletonKeeper {
     public static void main(String[] args) {
         LazySingleton lazySingleton = LazySingleton.getInstance();
